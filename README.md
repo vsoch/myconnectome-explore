d9b1f26cdd35821990275e184a4e9e909e76922d
vsoch <vsochat@stanford.edu>
2015-06-13 11:05:24 -0700

    Adding files for myconnectome-vm

---
+++ README.md
@@ -0,0 +1,58 @@
+### myconnectome-explore
+
+This code base provides a flask-driven infrastructure to explore the myconnectome project data, either integrated in the [myconnectome-vm]() or as a standalone application.
+
+#### myConnectome-vm
+If you are interested in reproducing all analyses in your own virtual machine, it is recommended to follow instructions in the [myconnectome-vm repository](). This vm will use myconnectome-explore to provide interactive exploration for the data that you produce. The installation is automatic and you do not need to do anything beyond following instructions to set up the virtual machine.
+
+### standalone application
+You should first clone this repository
+
+      git clone https://github.com/vsoch/myconnectome-explore
+      cd myconnectome-explore
+
+Install flask and flup
+
+      pip install flask
+      pip install flup
+
+And then run the server locally
+ 
+      python databrowser.py
+
+Don't worry about index.py - that is the executable that is integrated into the myconnectome-vm.
+
+### deployment
+
+1. Your deployment environment will need flask installed to the python that will be running the application. If you are creating a local python environment to run the application, we reccommend using anaconda, as it comes with most of the libraries that you should need,
+2. You should change "debug" "True" to debug "False" in the databrowser.py file.
+3. In the folder you need two files: an .htaccess and a myconnectome.fcgi. The myconnectome.fcgi should look as follows:
+
+
+      #!/home/youruser/local/anaconda/bin/python
+
+      from flup.server.fcgi import WSGIServer
+      from databrowser import app as application
+
+      WSGIServer(application).run()
+
+       
+The first line is the path to your python executable with flup and flask installed. 
+
+
+Your .htaccess should look like this:
+
+
+      Options +ExecCGI
+      AddHandler fcgid-script .fcgi 
+      RewriteEngine On
+      RewriteCond %{REQUEST_FILENAME} !=/home/youruser/public_html/myconnectome/myconnectome.fcgi
+      RewriteRule ^(.*)$ myconnectome.fcgi/$1 [QSA,L]
+
+The fourth line is the path to the myconnectome.fcgi
+
+4. Finally, you need to make your myconnectome.fcgi executable
+
+      chmod u+x myconnectome.fcgi
+
+That should be it! Going to the url yourserver/myconnectome should show the databrowser.
--- index.py
+++ index.py
@@ -1,84 +1,104 @@
-from flask import Flask, render_template
-from glob import glob
-import pandas
-import json
-import numpy
+from flask import Flask, render_template, redirect, Markup
+from flask.ext.autoindex import AutoIndex
+from subprocess import Popen
 import os
 
 app = Flask(__name__)
+index = AutoIndex(app, browse_root='/var/www/results',add_url_rules=False)
+
+@app.route('/results')
+@app.route('/results/<path:path>')
+def autoindex(path='.'):
+    return index.render_autoindex(path)
+
+@app.route('/log')
+def show_log():
+    err_log = read_log('/var/www/results/myconnectome/myconnectome_job.err')
+    out_log = read_log('/var/www/results/myconnectome/myconnectome_job.out')
+    return render_template('log.html',err_log=err_log,
+                                      out_log=out_log)
+
+def read_log(logfile):
+    logg = open(logfile,'rb').readlines()
+    logg = [l[0:-1] for l in logg]
+    logg = Markup('<br>'.join(logg))
+    return logg
 
 
-def get_lookup():
-    return {"metab":"metabolomics clustering",
-              "fullmetab":"single metabolites",
-              "wgcna":"weighted correlation network analysis",
-              "behav":"behavioral variables",
-              "netdata":"brain network measures",
-              "bwcorr":"between network correlation",
-              "wincorr":"within-network correlation",
-              "immport":"gene expression (immune)"}
-
-
-def prepare_data():
-
-    data_files = glob('results/myconnectome/timeseries/out*.txt')
-    data_files.sort()
-    dont_include = ['fd','pindex']
-
-    # Split files into different categories, subcategories
-    categories = [f.split('.')[2].split('_')[0] for f in data_files]
-    subcategories = [f.split('.')[2].split('_')[1] for f in data_files]
-
-    dropdown = dict()
-    for d in numpy.unique(categories):
-        if d not in dont_include:
-            dropdown[d] = [subcategories[x] for x in range(0,len(categories)) if categories[x]==d]
-
-    return dropdown
-
-# No variable selection
 @app.route('/')
-def data_chooser():
- 
-    dropdown = prepare_data()
+def show_analyses():
 
-    # Human interpretable labels
-    lookup = get_lookup()
-    
-    return render_template('explore.html',dropdown=dropdown,lookup=lookup)
+    timeseries_files = [('/var/www/results/myconnectome/timeseries/timeseries_analyses.html','Timeseries analyses'),
+                       ('/var/www/results/myconnectome/timeseries/Make_Timeseries_Heatmaps.html','Timeseries heatmaps'),
+                       ('/var/www/results/myconnectome/timeseries/Make_timeseries_plots.html','Timeseries plots'),
+                       ('/var/www/results/myconnectome/timeseries/behav_heatmap.pdf','Behavioral timeseries heatmap'),
+                       ('/var/www/results/myconnectome/timeseries/wincorr_heatmap.pdf','Within-network connectivity timeseries heatmap'),
+                       ('/var/www/results/myconnectome/timeseries/wincorr_heatmap.pdf','Within-network connectivity timeseries heatmap'),
+                       ('/var/www/results/myconnectome/timeseries/wgcna_heatmap.pdf','Gene expression module timeseries heatmap'),
+                       ('/var/www/results/myconnectome/timeseries','Listing of all files')]
+  rna_files =        [('/var/www/results/myconnectome/rna-seq/RNAseq_data_preparation.html','RNA-seq data preparation'),
+                       ('/var/www/results/myconnectome/rna-seq/Run_WGCNA.html','RNA-seq WGCNA analysis'),
+                       ('/var/www/results/myconnectome/rna-seq/snyderome/Snyderome_data_preparation.html','RNA-seq Snyderome analysis'),
+                       ('/var/www/results/myconnectome/rna-seq','Listing of all files')]
 
-# Variable selection
-@app.route('/<variable1>/<variable2>')
-def render_table(variable1,variable2):
- 
-    dropdown = prepare_data()
+    meta_files =       [('/var/www/results/myconnectome/metabolomics/Metabolomics_clustering.html','Metabolomics data preparation'),
+                        ('/var/www/results/myconnectome/metabolomics','Listing of all files')]
 
-    # Human interpretable labels
-    lookup = get_lookup()
-    table = make_table(variable1,variable2)
+    # How many green links should we have?
+    number_analyses = len(meta_files) + len(rna_files) + len(timeseries_files)
 
-    return render_template('explore.html',dropdown=dropdown,lookup=lookup,table=table)
+    # Check if the file exists, render context based on existence            
+    counter = 0
+    timeseries_context,counter = create_context(timeseries_files,counter)
+    rna_context,counter = create_context(rna_files,counter)
+    meta_context,counter = create_context(meta_files,counter)
 
+    # The counter determines if we've finished running analyses
+    analysis_status = 'Analysis is Running'
 
-# Read in a particular input file to render table
-def make_table(variable1,variable2):
+    # Check if the process is still running
+    process_running = check_process()
 
-    # Read in appropriate data file
-    data_file = 'results/myconnectome/timeseries/out.dat.%s_%s.txt' %(variable1,variable2)
-    tmp = pandas.read_csv(data_file,sep=" ")
-    
-    # Variables we want to save
-    xvar = tmp.xvar.tolist()
-    yvar = tmp.yvar.tolist()
-    corval = tmp["cor.val"].tolist()
-    tarima = tmp["t.arima"].tolist()
-    tdrift = tmp["t.drift"].tolist()
-    nobs = tmp.nobs.tolist()
-    pval_bh = tmp.pval_bh.tolist()
+    # If the process is not running
+    if process_running == False:
+        if counter == number_analyses:
+            analysis_status = 'Analysis Complete'
+        else:
+            analysis_status = 'Check for Error'
 
-    return zip(xvar,yvar,corval,tarima,tdrift,nobs,pval_bh)
+    return render_template('index.html',timeseries_context=timeseries_context,
+                                        rna_context=rna_context,
+                                        meta_context=meta_context,
+                                        analysis_status=analysis_status)
 
+# Check if python process is still running
+def check_process():
+    process_id = int(open('/home/vagrant/myconnectome/.started','rb').readlines()[0][0:-1])
+    try:
+        os.kill(process_id, 0)
+    except OSError:
+        return False
+    else:
+        return True
+
+def create_context(links,counter):
+    urls = []; descriptions = []; styles = []; titles = []
+    for link in links:
+        filename = link[0]
+        description = link[1]
+        if os.path.exists(filename):
+            counter+=1
+            urls.append(filename.replace('/var/www',''))
+            descriptions.append(description)
+            styles.append('color:rgb(25, 234, 25)')
+            titles.append(description)
+        else:
+            urls.append('#')
+            descriptions.append('%s %s' %(description,'(in progress)'))
+            styles.append('color:#ACBAC1;')
+            titles.append('PROCESSING')
+    return zip(urls,descriptions,styles,titles),counter
 
 if __name__ == '__main__':
-    app.debug = True
-    app.run()
+    app.debug = False
+    app.run(host='0.0.0.0')
